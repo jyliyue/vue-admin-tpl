e2cfe4e9a0e8247aff93a4e3ccacdf9662d0666a
jiangliyue <772748611@qq.com>
2019-05-03 16:58:46 +0800

    账号页面

---
+++ src/views/account/account.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="account-wrap">
+        account
+    </div>
+</template>
+
+<script>
+// import * from '@/api/accountApi'
+// import {  } from '@/components'
+export default {
+    name: 'account',
+    components: {
+        // ..
+    },
+    data() {
+        return {
+            // ..
+        }
+    },
+    created() {
+        // ..
+    },
+    mounted() {
+        // ..
+    },
+    methods: {
+        // ..
+    }
+}
+</script>
+
+<style lang="scss">
+@import './account.scss';
+</style>
